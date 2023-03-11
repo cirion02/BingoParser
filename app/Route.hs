641479436d43e78b64016fc20e525fe585898b45
cirion02 <34488326+cirion02@users.noreply.github.com>
2023-03-11 17:56:35 +0100

    Added pico/epilogue support

--- app/Route.hs
+++ app/Route.hs
@@ -2,6 +2,7 @@ module Route (blueHeartCount, showRouteBreakdown,
 routeCheckpointSet, routeCheckpointSetMultiple, 
 routeAllCheckpointsSet, routeAllCheckpointsSetMultiple,
 setCassette, setComplete, setHeart, addBerries, addBinos, addTasks,
+addPicoBerries, setPicoComplete, setPicoOrb, setPicoSite, setBirdsNest,
 emptyRoute, Route) where
 
 import Helper
@@ -25,14 +26,25 @@ data Checkpoint = Checkpoint Name Complete Berries Cassette Heart Binos OtherTas
 
 data Chapter = Chapter Name [Checkpoint]
 
-data Route = Route ASides BSides Time
+
+type ReachOrb = Bool
+type ReachSite = Bool
+type BirdsNest = Bool
+
+data Pico = Pico Complete ReachOrb ReachSite Berries
+data Epilogue = Epilogue BirdsNest 
+
+data OtherLocations = OtherLocations Pico Epilogue
+
+data Route = Route ASides BSides OtherLocations Time
 
 
-type RouteAlgebra route asides bsides chap check name time ber bino cas hea com tasks res = (
+type RouteAlgebra route asides bsides chap check name time ber bino cas hea com tasks orb site nest loc locs res = (
   route -> res,
-  asides -> bsides -> time -> route,
+  asides -> bsides -> locs -> time -> route,
   [chap] -> asides,
   [chap] -> bsides,
+  loc -> loc -> locs,
   name -> [check] -> chap,
   name -> com -> ber -> cas -> hea -> bino -> tasks -> check,
   Name -> name,
@@ -42,26 +54,25 @@ type RouteAlgebra route asides bsides chap check name time ber bino cas hea com 
   Heart -> hea,
   Complete -> com,
   Binos -> bino,
-  OtherTasks -> tasks
+  OtherTasks -> tasks,
+  ReachOrb -> orb,
+  ReachSite -> site,
+  BirdsNest -> nest,
+  com -> orb -> site -> ber -> loc,
+  nest -> loc
   )
 
-fold :: RouteAlgebra route asides bsides chap check name time ber bino cas hea com tasks res -> Route -> res
-fold (resF, routeF, asideF, bsideF, chapF, checkF, nameF, timeF, berF, casF, heaF, comF, binoF, tasksF) = fres
+fold :: RouteAlgebra route asides bsides chap check name time ber bino cas hea com tasks orb site nest loc locs res -> Route -> res
+fold (resF, routeF, asideF, bsideF, olocF, chapF, checkF, nameF, timeF, berF, casF, heaF, comF, binoF, tasksF, orbF, siteF, nestF, picoF, epilF) = fres
   where
-    fres (Route a b t) = resF $ routeF (asideF $ map fchap a) (bsideF $ map fchap b) (timeF t)
+    fres (Route a b (OtherLocations p e) t) = resF $ routeF (asideF $ map fchap a) (bsideF $ map fchap b) (olocF (fpico p) (fepil e)) (timeF t)
 
     fchap (Chapter n cps) = chapF (nameF n) (map fcheck cps)
 
     fcheck (Checkpoint n co be ca h bi t) = checkF (nameF n) (comF co) (berF be) (casF ca) (heaF h) (binoF bi) (tasksF t)
 
-first3 :: a -> b -> c -> a
-first3  x _ _ = x
-
-second3 :: a -> b -> c -> b
-second3 _ x _ = x
-
-third3 :: a -> b -> c -> c
-third3  _ _ x = x
+    fpico (Pico c o s b) = picoF (comF c) (orbF o) (siteF s) (berF b)
+    fepil (Epilogue n) = epilF (nestF n)
 
 ignore :: a -> ()
 ignore _ = ()
@@ -76,30 +87,40 @@ boolToInt False = 0
 blueHeartCount :: Route -> Int
 blueHeartCount = fold alg
   where
-    alg :: RouteAlgebra Int Int () Int Int () () () () () Int () () Int
+    alg :: RouteAlgebra Int Int () Int Int () () () () () Int () () () () () () () Int
     alg = (
         id, 
-        first3, 
+        \x _ _ _ -> x, 
         sum, 
         ignore,
+        ignore2,
         \_ xs -> sum xs,
         \_ _ _ _ x _ _ -> x,
-        ignore, ignore, ignore, ignore,
+        ignore, 
+        ignore, 
+        ignore, 
+        ignore,
         boolToInt,
         ignore,
         ignore,
+        ignore,
+        ignore,
+        ignore,
+        ignore,
+        \_ _ _ _ -> (),
         ignore
       )
 
 showRouteBreakdown :: Route -> String
 showRouteBreakdown = fold alg
   where
-    alg :: RouteAlgebra String String String String String String String [String] [String] Bool Bool Bool [String] String
+    alg :: RouteAlgebra String String String String String String String [String] [String] Bool Bool Bool [String] Bool Bool Bool String String String
     alg = (
         id,
-        \a b t -> a ++ "\n\n\n\n" ++ b ++ "\n\n\n\n" ++ t,
+        \a b o t -> a ++ "\n\n\n" ++ b ++ "\n\n\n" ++ if' (null o) "" (o ++ "\n\n\n") ++ t,
         \cs -> "A-sides:\n\n" ++ intercalate "\n\n" (filter (/= "") cs),
         \cs -> "B-sides:\n\n" ++ intercalate "\n\n" (filter (/= "") cs),
+        \pic epi -> if' (any (/= "") [pic, epi]) ("Other Locations:\n\n" ++ intercalate "\n" (filter (/= "") [pic, epi])) "",
         showChapter,
         showCheckPoint,
         id,
@@ -109,12 +130,25 @@ showRouteBreakdown = fold alg
         id,
         id,
         map show . Set.elems,
-        Set.elems
+        Set.elems,
+        id,
+        id,
+        id,
+        showPico,
+        \n -> if' n "Epilogue:\n-Visit Birds Nest" ""
       )
 
+      {-
+      ReachOrb -> orb,
+  ReachSite -> site,
+  BirdsNest -> nest,
+  com -> orb -> site -> ber -> loc,
+  nest -> loc
+  -}
+
     showChapter :: String -> [String] -> String
-    showChapter n cs | not $ any (/= "") cs = ""
-                     | otherwise            = n ++ ":\n" ++ intercalate "\n" (filter (/= "") cs)
+    showChapter n cs | any (/= "") cs = n ++ ":\n" ++ intercalate "\n" (filter (/= "") cs)
+                     | otherwise      = ""
 
     showCheckPoint :: String -> Bool -> [String] -> Bool -> Bool -> [String] -> [String] -> String
     showCheckPoint name complete berries cassette heart binos tasks | checkpointIsEmpty name complete berries cassette heart binos tasks = ""
@@ -129,6 +163,13 @@ showRouteBreakdown = fold alg
         if' heart "Heart" "",
         if' (null binos) "" ("Binos: " ++ unwords binos)
       ] ++ tasks)
+
+    showPico :: Bool -> Bool -> Bool -> [String] -> String
+    showPico True _ _ berries       = "Pico-8:\n-Complete" ++ if' (null berries) "" ("Berries: " ++ unwords berries)
+    showPico _ True _ berries       = "Pico-8:\n-Get the orb" ++ if' (null berries) "" ("Berries: " ++ unwords berries)
+    showPico _ _ True berries       = "Pico-8:\n-Reach Old Site" ++ if' (null berries) "" ("Berries: " ++ unwords berries)
+    showPico _ _ _    berries@(_:_) = "Pico-8:\nBerries: " ++ unwords berries
+    showPico _ _ _    _             = ""
     
     checkpointIsEmpty :: String -> Bool -> [String] -> Bool -> Bool -> [String] -> [String] -> Bool
     checkpointIsEmpty _ True _ _ _ _ _ = False
@@ -143,14 +184,14 @@ showRouteBreakdown = fold alg
     checkpointOnlyComplete _ _ [] False False [] [] = True
     checkpointOnlyComplete _ _ _ _ _ _ _ = False
 
-routeAllCheckpointsSetMultiple :: Bool -> Int -> Route -> [Checkpoint -> Checkpoint] -> Route
-routeAllCheckpointsSetMultiple aside chapId = foldr routeSetFunc
+routeAllCheckpointsSetMultiple :: Bool -> Int -> [Checkpoint -> Checkpoint] -> Route -> Route
+routeAllCheckpointsSetMultiple aside chapId = flip $ foldr routeSetFunc
   where
     routeSetFunc = routeAllCheckpointsSet aside chapId
 
 routeAllCheckpointsSet :: Bool -> Int -> (Checkpoint -> Checkpoint) -> Route -> Route
-routeAllCheckpointsSet True chapId cpFunc (Route asides bsides time) = Route (setAllCheckpointsChapters' chapId cpFunc asides) bsides time
-routeAllCheckpointsSet False chapId cpFunc (Route asides bsides time) = Route asides (setAllCheckpointsChapters' chapId cpFunc bsides) time
+routeAllCheckpointsSet True chapId cpFunc (Route asides bsides other time) = Route (setAllCheckpointsChapters' chapId cpFunc asides) bsides other time
+routeAllCheckpointsSet False chapId cpFunc (Route asides bsides other time) = Route asides (setAllCheckpointsChapters' chapId cpFunc bsides) other time
 
 setAllCheckpointsChapters' :: Int -> (Checkpoint -> Checkpoint) -> [Chapter] -> [Chapter]
 setAllCheckpointsChapters' chapId cpFunc chaps = setAt (chapId-1) (setAllCheckpointsChapter' cpFunc (chaps !! (chapId-1))) chaps
@@ -158,14 +199,14 @@ setAllCheckpointsChapters' chapId cpFunc chaps = setAt (chapId-1) (setAllCheckpo
 setAllCheckpointsChapter' :: (Checkpoint -> Checkpoint) -> Chapter -> Chapter
 setAllCheckpointsChapter' cpFunc (Chapter n cps) = Chapter n $ map cpFunc cps
 
-routeCheckpointSetMultiple :: Bool -> Int -> Int -> Route -> [Checkpoint -> Checkpoint] -> Route
-routeCheckpointSetMultiple aside chapId cpId = foldr routeSetFunc
+routeCheckpointSetMultiple :: Bool -> Int -> Int -> [Checkpoint -> Checkpoint] -> Route -> Route
+routeCheckpointSetMultiple aside chapId cpId = flip $ foldr routeSetFunc
   where
     routeSetFunc = routeCheckpointSet aside chapId cpId 
 
 routeCheckpointSet :: Bool -> Int -> Int -> (Checkpoint -> Checkpoint) -> Route -> Route
-routeCheckpointSet True chapId cpId cpFunc (Route asides bsides time) = Route (setChapters' chapId cpId cpFunc asides) bsides time
-routeCheckpointSet False chapId cpId cpFunc (Route asides bsides time) = Route asides (setChapters' chapId cpId cpFunc bsides) time
+routeCheckpointSet True chapId cpId cpFunc (Route asides bsides other time) = Route (setChapters' chapId cpId cpFunc asides) bsides other time
+routeCheckpointSet False chapId cpId cpFunc (Route asides bsides other time) = Route asides (setChapters' chapId cpId cpFunc bsides) other time
 
 setChapters' :: Int -> Int -> (Checkpoint -> Checkpoint) -> [Chapter] -> [Chapter]
 setChapters' chapId cpId cpFunc chaps = setAt (chapId-1) (setChapter' cpId cpFunc (chaps !! (chapId-1))) chaps
@@ -194,6 +235,21 @@ addBinos val (Checkpoint n f bs c h bis ts) = Checkpoint n f bs c h (Set.union b
 addTasks :: [String] -> Checkpoint -> Checkpoint
 addTasks val (Checkpoint n f bs c h bis ts) = Checkpoint n f bs c h bis (Set.union ts $ Set.fromList val)
 
+addPicoBerries :: [Int] -> Route -> Route
+addPicoBerries val (Route as bs (OtherLocations (Pico c o s bes) ep) t) = (Route as bs (OtherLocations (Pico c o s (Set.union bes $ Set.fromList val)) ep) t)
+
+setPicoComplete :: Bool -> Route -> Route
+setPicoComplete val (Route as bs (OtherLocations (Pico _ o s bes) ep) t) = (Route as bs (OtherLocations (Pico val o s bes) ep) t)
+
+setPicoOrb :: Bool -> Route -> Route
+setPicoOrb val (Route as bs (OtherLocations (Pico c _ s bes) ep) t) = (Route as bs (OtherLocations (Pico c val s bes) ep) t)
+
+setPicoSite :: Bool -> Route -> Route
+setPicoSite val (Route as bs (OtherLocations (Pico c o _ bes) ep) t) = (Route as bs (OtherLocations (Pico c o val bes) ep) t)
+
+setBirdsNest :: Bool -> Route -> Route
+setBirdsNest val (Route as bs (OtherLocations p (Epilogue _)) t) = (Route as bs (OtherLocations p (Epilogue val)) t)
+
 {-
 type ASides = [Chapter]
 type BSides = [Chapter]
@@ -221,8 +277,7 @@ emptyASides = [
     createEmptyChapter "Reflection" ["Start", "Lake", "Hollows", "Reflection", "Rock Bottom", "Resolution"],
     createEmptyChapter "Summit" ["0M", "500M", "1000M", "1500M", "2000M", "2500M", "3000M"],
     createEmptyChapter "Core" ["Start", "Into the Core", "Hot and Cold", "Heart of the Mountain"],
-    createEmptyChapter "Farewell" ["Start", "Singular", "Power Source", "Remembered", "Event Horizon", "The rest of farewell"],
-    createEmptyChapter "Epilogue" ["Epilogue"]
+    createEmptyChapter "Farewell" ["Start", "Singular", "Power Source", "Remembered", "Event Horizon", "The rest of farewell"]
   ]
 
 emptyBSides :: BSides
@@ -235,9 +290,12 @@ emptyBSides = [
     createEmptyChapter "Reflection" ["Start", "Reflection", "Rock Bottom", "Reprieve"],
     createEmptyChapter "Summit" ["0M", "500M", "1000M", "1500M", "2000M", "2500M", "3000M"]
   ]
+
+emptyOtherLocations :: OtherLocations
+emptyOtherLocations = OtherLocations (Pico False False False Set.empty) (Epilogue False)
     
 emptyRoute :: Route
-emptyRoute = Route emptyASides emptyBSides 0
+emptyRoute = Route emptyASides emptyBSides emptyOtherLocations 0
 
 
 {-
